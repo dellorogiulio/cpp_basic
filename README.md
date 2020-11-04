47b93b58a3f78f5c57e4c6c9ec1b1d14422027c5
Giulio Dell'Oro <dellorogiulio@gmail.com>
2020-11-04 08:28:43 +0100

    Add concepts/namespaces

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cpp_basic
 Some Basic C++ concepts and examples
+
+As writing rules, please follow https://google.github.io/styleguide/cppguide.html
---
+++ concepts/1_namespaces.cpp
@@ -0,0 +1,92 @@
+/* 
+ --- NAMESPACES ---
+		Namespaces are a sort of container in which everything is declaired in remains 
+		in it.
+		Namespaces are useful to keep the code clean and organize and they "allow" function
+		redefinitions.
+ */
+
+namespace namespace1  // here a namespace declaration
+{                     // no need to indent in a namespace
+int function1()
+{
+    return 1;
+}
+
+int function2()
+{
+    return 2;
+}
+}  // namespace namespace1
+/* no need to semicolon at the end of namespace */
+
+namespace namespace2
+{
+int function1()
+{
+    return 1;
+}
+
+int function2()
+{
+    return 2;
+}
+}  // namespace namespace2
+
+namespace namespace3
+{
+/*
+	Obviously not only function can be define in a namespace;
+	Classes, variables, functions... everything can be define in a namespace
+*/
+const int value = 2;
+
+// And you can nest namespaces in a namespace
+namespace namespace3_inner
+{
+const int value = 3;
+}
+}  // namespace namespace3
+
+int main()
+{
+    int n1 = namespace1::function1();
+    int n2 = namespace2::function1();
+    /*  Note that function1 has the same signature in namespace1 and namespace2 but,
+		since they are "confined" in the namespace, there is no ambiguity
+	*/
+
+    {
+        /*	It is possible to "keep up" everything which is a namespace to the current scope
+			by using the keyword "using namespace"
+		*/
+        using namespace namespace1;
+        /* 	now everything which is contained in namespace1 is available in the current scope
+			without using "namespace1::"
+		*/
+        int n3 = function1();  // no ambiguity: function1() without anything in front of it can be only namespace1::function1
+                               // no need to use "namespace1::" since everything in it is accesible here
+    }
+    /*	Note that now everything in namespace1 is "packed back" in the namespace and is NO more 
+		accessible without the "namespace1::" indication */
+
+    // THIS IS A COMPILE ERROR
+    // int n4 = function1(); -> 'function1' was not declared in this scope
+
+    /* you can obviously use more than one "using namespace" sintax, but pay attention */
+    using namespace namespace1;
+    using namespace namespace2;
+    /* Now everything in namespace1 and namespace2 is accessible to the current scope */
+
+    // THIS IS A COMPILE ERROR
+    // int n5 = function1(); ->  call of overloaded 'function1()' is ambiguous
+    /* the compiler is in fact no more able to decide which function1 use, since namespace1::function1 and
+	   namespace2::function1 are both visible in the current scope
+	*/
+
+    /*  It is possible to access namespace members with no limit */
+    const int n6 = namespace3::value;
+    const int n7 = namespace3::namespace3_inner::value;
+
+    return 0;
+}
