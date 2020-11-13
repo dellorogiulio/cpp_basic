e9f2eb99fad16e3edf9a04f7564a6d1600bf9121
escherstair <ernestviga@gmail.com>
2020-11-13 14:39:44 +0100

    Improved some comments

--- concepts/7_classes_part_1.cpp
+++ concepts/7_classes_part_1.cpp
@@ -1,7 +1,7 @@
 /* 
  --- CLASSES PART 1 ---
 		Classes are fundamental part of the language, since C++ is typically called a "Object-oriented language".
-        Classes allow to keep data and logic "pack" in a single point.
+        Classes allow to keep data and logic "packed together" in a single point.
 */
 
 /*
@@ -25,7 +25,7 @@ struct Person
     std::string surname;
 };
 
-std::string getfullName(Person* person)
+std::string getfullName(const Person* person)		// note the const pointer since getfullName() doesn't change the input parameter person
 {
     return person->name + " " + person->surname;
 }
@@ -33,7 +33,8 @@ std::string getfullName(Person* person)
 
 void cStylePerson()
 {
-    c_style::Person person = {.name = "Mario", .surname = "Rossi"};  // declaration and initialization
+    c_style::Person person = {.name = "Mario", .surname = "Rossi"};  // declaration and initialization (i.e. memory is allocated for 'person' and it's filled
+								     // note the C99 "designated initializers" used to initialize 'person' (.name = xxx .surname = yyy)
     auto full_name = c_style::getfullName(&person);                  // call function on the instance
 }
 
@@ -43,19 +44,19 @@ namespace cpp_style
 {
 struct Person
 {
-    std::string name;  // this is called "class attribute"
+    std::string name;  // this is called "class attribute" (or "class member")
     std::string surname;
 
     // it is possible to declare a function which 'lives' only inside the class, this is called "class method"
     std::string fullName()  // <- no arguments
     {
-        return name + " " + surname;  // see how it is possible to access directly the 'name' and the 'surname' attributes
+        return name + " " + surname;  // see how inside this struct it is possible to access directly the 'name' and the 'surname' attributes (members) through their names
     }
 
     std::string getName()
     {
         return this->name;  // what's 'this'? See later!
-        // of course, return name; will be ok: name and this->name are exactly the same!
+        // of course, writing "return name;" is ok too: 'name' and 'this->name' are exactly the same!
     }
 };
 }  // namespace cpp_style
@@ -64,15 +65,16 @@ void cppStylePerson()
 {
     cpp_style::Person person = {.name = "Mario", .surname = "Rossi"};
     auto full_name = person.fullName();
-    // see how fullName() can be accessed directly by the person instance.
+    // see how fullName() can be called directly over the 'person' instance.
 
     /*  But how this works?
-        the compiler see the fullName() method as fullName(Person* this)
-        Every class method has in fact an "hidden" parameter (the first one) which is a pointer to the class instance.
-        the call of person.fullName() "becomes" fullName(&person) as seen by the compiler.     
+        the compiler sees the signature of class method fullName() as fullName(Person* this)
+        The signature of every class method has in fact a "hidden" parameter (the first one) which is a pointer to the class instance.
+	This hidden parameter is called 'this'
+        The call of person.fullName() "becomes" fullName(&person) from the compiler "point of view".
     */
     auto name = person.getName();
-    // In the name() method, a variable 'this' is in fact accessible. Remember that 'this' is of type Person*
+    // Now you understand why in the body of getName() method, a variable 'this' is accessible. Remember that 'this' is of type Person*
 }
 
 int main()
